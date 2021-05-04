d8c72806596babffb8f23c44a7d905afb01aca68
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-05-05 01:16:41 +0500

    add: added the restaurant to DB
    Создали таблицу в БД c первичным ключом id, используя typeorm

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,13 +1,34 @@
 import { Field, ObjectType } from '@nestjs/graphql'
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
 
-@ObjectType()
+/* Field для GraphQL,
+ * Column & Primary... для typeorm и PostgreSQL
+ * */
+
+@ObjectType() // -- graphql --
+@Entity() // -- typeorm --
 export class Restaurant {
+    @Field((type) => Number)
+    @PrimaryGeneratedColumn() // первичный коюч
+    id: number
+
     @Field((type) => String)
+    @Column()
     name: string
+
     @Field((type) => Boolean)
+    @Column()
     isVegan: boolean
+
     @Field((type) => String)
+    @Column()
     address: string
+
     @Field((type) => String)
+    @Column()
     ownName: string
+
+    @Field((type) => String)
+    @Column()
+    categoryName: string
 }
