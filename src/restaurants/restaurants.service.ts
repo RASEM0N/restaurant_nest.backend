e38a2925c67aee331aaf647e30a43097730c8b83
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-05-06 00:38:40 +0500

    add: обновление ресторана

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -3,6 +3,7 @@ import { Restaurant } from './entities/restaurants.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto'
+import { UpdateRestaurantDto } from './dtos/update-restaurant.dto'
 
 @Injectable()
 export class RestaurantsService {
@@ -18,5 +19,9 @@ export class RestaurantsService {
     createRestaurant(createRestaurantDto: CreateRestaurantDto): Promise<Restaurant> {
         const newRestaurant = this.restaurants.create(createRestaurantDto)
         return this.restaurants.save(newRestaurant)
+    }
+
+    updateRestaurant({ id, data }: UpdateRestaurantDto) {
+        return this.restaurants.update(id, { ...data })
     }
 }
