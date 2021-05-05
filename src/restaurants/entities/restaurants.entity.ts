fc368703b4652d422263c0972f04aee328abb9a3
RASEM0N <70789810+RASEM0N@users.noreply.github.com>
2021-05-05 14:28:20 +0500

    ...

--- src/restaurants/entities/restaurants.entity.ts
+++ src/restaurants/entities/restaurants.entity.ts
@@ -1,10 +1,13 @@
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, InputType, ObjectType } from '@nestjs/graphql'
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+import { IsBoolean, IsString, Length } from 'class-validator'
 
 /* Field для GraphQL,
  * Column & Primary... для typeorm и PostgreSQL
  * */
-
+@InputType({
+    isAbstract: true,
+})
 @ObjectType() // -- graphql --
 @Entity() // -- typeorm --
 export class Restaurant {
@@ -14,21 +17,27 @@ export class Restaurant {
 
     @Field((type) => String)
     @Column()
+    @IsString()
+    @Length(5, 10)
     name: string
 
     @Field((type) => Boolean)
     @Column()
+    @IsBoolean()
     isVegan: boolean
 
     @Field((type) => String)
     @Column()
+    @IsString()
     address: string
 
     @Field((type) => String)
     @Column()
+    @IsString()
     ownName: string
 
     @Field((type) => String)
     @Column()
+    @IsString()
     categoryName: string
 }
